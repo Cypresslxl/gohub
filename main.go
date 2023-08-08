e3d2dfd9a13252dee7509ef96cc898cc0e4381f7
Cypresslxl <laixinlianglxl@icloud.com>
2023-08-08 18:37:29 +0800

    配置方案的实现

--- main.go
+++ main.go
@@ -1,26 +1,38 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"gohub/bootstrap"
+	btsConfig "gohub/config"
+	"gohub/pkg/config"
 
 	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	// 加载 config 目录下的配置信息
+	btsConfig.Initialize()
+}
+
 func main() {
-	// r := gin.Default() //Default 返回的是一个 Engine 对象。且默认帮我们注册了两个中间件，Logger 和 Recovery 中间件。
-	r := gin.New() //* gin.Engine
+	// 配置初始化，依赖命令行 --env 参数
 
-	// register middleware
-	// r.Use(gin.Logger(), gin.Recovery())
-	//程序初始化，初始化路由绑定
-	bootstrap.SetupRoute(r)
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
 
-	err := r.Run(":8848")
+	// new 一个 Gin Engine 实例
+	router := gin.New()
+
+	// 初始化路由绑定
+	bootstrap.SetupRoute(router)
+
+	// 运行服务
+	err := router.Run(":" + config.Get("app.port"))
 	if err != nil {
-		//错误处理，端口被占用了或者其他错误
-
+		// 错误处理，端口被占用了或者其他错误
 		fmt.Println(err.Error())
 	}
-	// r.Run() 默认端口为8080
 }
---
+++ pkg/config/config.go
@@ -0,0 +1,149 @@
+package config
+
+import (
+	"gohub/pkg/helpers"
+	"os"
+
+	"github.com/spf13/cast"
+	viperlib "github.com/spf13/viper"
+)
+
+// viper 库实例
+var viper *viperlib.Viper
+
+// Configue 动态加载配置信息
+type ConfigFunc func() map[string]interface{}
+
+// 先加载到此数组，loadConfig 再动态生成配置信息
+var ConfigFuncs map[string]ConfigFunc
+
+func init() {
+	// 1.初始化Viper库
+	viper = viperlib.New()
+	// 2.配置类型支持 "json", "toml", "yaml", "yml", "properties",
+	//             "props", "prop", "env", "dotenv"
+	viper.SetConfigType("env")
+
+	// 3.环境变量配置文件查找的路径，相对于 main.go
+	viper.AddConfigPath(".")
+	// 4. 设置环境变量前缀，用以区分 Go 的系统环境变量
+	viper.SetEnvPrefix("appenv")
+	// 5. 读取环境变量（支持 flags）
+	viper.AutomaticEnv()
+
+	ConfigFuncs = make(map[string]ConfigFunc)
+}
+
+// InitConfig 初始化配置信息，完成对环境变量以及 config 信息的加载
+func InitConfig(env string) {
+	// 1. 加载环境变量
+	loadEnv(env)
+	// 2. 注册配置信息
+	loadConfig()
+}
+
+func loadConfig() {
+	for name, fn := range ConfigFuncs {
+		// viper.Set(name, fn)
+		viper.Set(name, fn()) //这里为什么要加括号
+	}
+}
+
+func loadEnv(envSuffix string) {
+	// 默认加载 .env 文件，如果有传参 --env=name 的话，加载 .env.name 文件
+	envPath := ".env"
+	if len(envSuffix) > 0 {
+		filename := envPath + envSuffix
+		if _, err := os.Stat(filename); err == nil { //os.Stat() 的作用：It returns an os.FileInfo struct that contains various information about the file or directory.
+			// 如 .env.testing 或 .env.stage
+			envPath = filename
+		}
+	}
+
+	// 加载 env
+	viper.SetConfigName(envPath)
+	if err := viper.ReadInConfig(); err != nil {
+		//ReadInConfig will discover and load the configuration file from disk
+		// and key/value stores, searching in one of the defined paths.
+		panic(err)
+	}
+
+	// 监控 .env 文件，变更时重新加载
+	viper.WatchConfig()
+}
+
+// Env 读取环境变量，支持默认值
+func Env(envName string, defaultValue ...interface{}) interface{} {
+	if len(defaultValue) > 0 {
+		return internalGet(envName, defaultValue[0])
+	}
+	return internalGet(envName)
+}
+
+// Add 新增配置项
+func Add(name string, configFn ConfigFunc) {
+	ConfigFuncs[name] = configFn
+}
+
+// Get 获取配置项
+// 第一个参数 path 允许使用点式获取，如：app.name
+// 第二个参数允许传参默认值
+// func Get(path string, defaultValue ...interface{}) string {
+// return GetString(path, defaultValue...)
+// }
+
+func Get(path string, defaultValue ...interface{}) string {
+	return GetString(path, defaultValue...)
+}
+
+// In summary, this function is designed to retrieve configuration values using Viper,
+// with the option to provide a default value if the requested value is missing or empty 1
+// If no default value is provided, the function returns nil in such cases.
+func internalGet(path string, defaultValue ...interface{}) interface{} {
+	// Check if the configuration key exists or if its value is empty
+	if !viper.IsSet(path) || helpers.Empty(viper.Get(path)) {
+		// If a default value is provided and there is at least one element in defaultValue slice
+		if len(defaultValue) > 0 {
+			return defaultValue[0] // Return the provided default value
+		}
+		return nil // Return nil since there's no default value
+	}
+	// Return the value from the configuration
+	return viper.Get(path)
+}
+
+// next
+// GetString 获取 String 类型的配置信息
+func GetString(path string, defaultValue ...interface{}) string {
+	return cast.ToString(internalGet(path, defaultValue...))
+}
+
+// GetInt 获取 Int 类型的配置信息
+func GetInt(path string, defaultValue ...interface{}) interface{} {
+	return cast.ToInt(internalGet(path, defaultValue...))
+}
+
+// GetFloat64 获取Float64 类型的配置信息
+func GetFloat64(path string, defaultValue ...interface{}) interface{} {
+	return cast.ToFloat64(internalGet(path, defaultValue...))
+}
+
+// GetInt64 获取 Int64 类型的配置信息
+func GetInt64(path string, defaultValue ...interface{}) interface{} {
+	return cast.ToInt64(internalGet(path, defaultValue...))
+}
+
+// GetUint 获取 Uint 类型的配置信息
+func GetUint(path string, defaultValue ...interface{}) interface{} {
+	return cast.ToUint(internalGet(path, defaultValue...))
+}
+
+// GetBool 获取 Bool 类型的配置信息
+func GetBool(path string, defaultValue ...interface{}) interface{} {
+	return cast.ToBool(internalGet(path, defaultValue...))
+}
+
+// GetStringMapString 获取结构数据
+func GetStringMapString(path string) map[string]string {
+	return viper.GetStringMapString(path)
+}
