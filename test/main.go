52bc0ba65a42f2507198d88e7d3c15a1a52050ab
Cypresslxl <laixinlianglxl@icloud.com>
2023-08-09 17:45:53 +0800

    数据库请求日志

--- test/main.go
+++ test/main.go
@@ -1,25 +1,17 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
+
+func change(s ...int) {
+	s = append(s, 3)
+}
 
 func main() {
-	// var env string
-
-	// Define a string flag named "env"
-	// flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
-
-	// Parse command-line arguments
-	// flag.Parse()
-
-	// Print the value of the "env" flag
-	// fmt.Println("Environment:", env)
-	str := "broken pipe"
-	if ok := strings.Contains(str, "ken"); !ok {
-		fmt.Println("not ok")
-	} else {
-		fmt.Println("ok")
-	}
+	slice := make([]int, 5, 5)
+	slice[0] = 1
+	slice[1] = 2
+	change(slice...)
+	fmt.Println(slice)
+	change(slice[0:2]...)
+	fmt.Println(slice)
 }
