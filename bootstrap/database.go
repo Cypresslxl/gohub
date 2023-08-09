ca8d81a91dde006a1cc1bce7585dd855d6c95565
Cypresslxl <laixinlianglxl@icloud.com>
2023-08-09 11:01:13 +0800

    手机接口已经注册

--- bootstrap/database.go
+++ bootstrap/database.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"gohub/pkg/config"
 	"gohub/pkg/database"
+
+	"gohub/app/models/user" //是这个，而不是// "os/user"
 	"time"
 
 	"gorm.io/driver/mysql"
@@ -47,4 +49,6 @@ func SetupDB() {
 	database.SQLDB.SetMaxIdleConns(config.GetInt("database.mysql.max_idle_connections"))
 	// 设置每个链接的过期时间
 	database.SQLDB.SetConnMaxLifetime(time.Duration(config.GetInt("database.mysql.max_life_seconds")) * time.Second)
+
+	database.DB.AutoMigrate(&user.User{})
 }
--- routes/api.go
+++ routes/api.go
@@ -1,7 +1,7 @@
 package routes
 
 import (
-	"net/http"
+	"gohub/app/http/controllers/api/v1/auth"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,12 +12,18 @@ func RegisterAPIRoutes(r *gin.Engine) {
 	// 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
 	v1 := r.Group("/v1")
 	{
-		v1.GET("/", func(c *gin.Context) {
-			//JSON 格式相应
-			c.JSON(http.StatusOK, gin.H{
-				"code":    1,
-				"message": "this is v1",
-			})
-		})
+		authGroup := v1.Group("/auth")
+		{
+			obj := new(auth.SignupController)
+			// 判断手机号是否已经注册
+			authGroup.POST("/signup/phone/exist", obj.IsPhoneExist)
+		}
+		// v1.GET("/", func(c *gin.Context) {
+		// JSON 格式相应
+		// c.JSON(http.StatusOK, gin.H{
+		// "code":    1,
+		// "message": "this is v1",
+		// })
+		// })
 	}
 }
