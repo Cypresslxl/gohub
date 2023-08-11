b3a7a2adc6686141e34e48931b08e586cba739da
Cypresslxl <laixinlianglxl@icloud.com>
2023-08-11 20:41:26 +0800

    key 命令

--- app/cmd/serve.go
+++ app/cmd/serve.go
@@ -24,7 +24,8 @@ func runWeb(cmd *cobra.Command, args []string) {
 	// release 会屏蔽调试信息，官方建议生产环境中使用
 	// 非 release 模式 gin 终端打印太多信息，干扰到我们程序中的 Log
 	// 故此设置为 release，有特殊情况手动改为 debug 即可
-	gin.SetMode(gin.ReleaseMode)
+	//gin.SetMode(gin.ReleaseMode)
+	gin.SetMode(gin.DebugMode)
 
 	// gin 实例
 	router := gin.New()
