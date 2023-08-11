b4565a946bda4cd3ace9e5aab7d157595832dd87
Cypresslxl <laixinlianglxl@icloud.com>
2023-08-11 17:24:48 +0800

    限流中间件

--- go.mod
+++ go.mod
@@ -5,14 +5,17 @@ go 1.20
 require (
 	github.com/KenmyZhang/aliyun-communicate v0.0.0-20180308134849-7997edc57454
 	github.com/gin-gonic/gin v1.9.1
-	github.com/go-redis/redis/v8 v8.11.5
+	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/jordan-wright/email v4.0.1-0.20210109023952-943e75fe5223+incompatible
 	github.com/mojocn/base64Captcha v1.3.5
 	github.com/natefinch/lumberjack v2.0.0+incompatible
+	github.com/redis/go-redis/v9 v9.0.5
 	github.com/spf13/cast v1.5.1
 	github.com/spf13/viper v1.16.0
 	github.com/thedevsaddam/govalidator v1.9.10
+	github.com/ulule/limiter/v3 v3.11.2
 	go.uber.org/zap v1.25.0
+	golang.org/x/crypto v0.12.0
 	gorm.io/driver/mysql v1.5.1
 	gorm.io/driver/sqlite v1.5.2
 	gorm.io/gorm v1.25.2
@@ -20,7 +23,7 @@ require (
 
 require (
 	github.com/bytedance/sonic v1.10.0-rc3 // indirect
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/chenzhuoyu/iasm v0.9.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
@@ -32,7 +35,6 @@ require (
 	github.com/go-playground/validator/v10 v10.15.0 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
-	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/google/uuid v1.1.2 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
@@ -49,6 +51,7 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pborman/uuid v1.2.1 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.9 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/spf13/afero v1.9.5 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
@@ -57,7 +60,6 @@ require (
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/arch v0.4.0 // indirect
-	golang.org/x/crypto v0.12.0 // indirect
 	golang.org/x/image v0.0.0-20190802002840-cff245a6509b // indirect
 	golang.org/x/net v0.14.0 // indirect
 	golang.org/x/sys v0.11.0 // indirect
@@ -65,5 +67,6 @@ require (
 	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- routes/api.go
+++ routes/api.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"gohub/app/http/controllers/api/v1/auth"
+	"gohub/app/http/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,47 +13,59 @@ func RegisterAPIRoutes(r *gin.Engine) {
 	// 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
 	v1 := r.Group("/v1")
 	{
-		authGroup := v1.Group("/auth")
+		// 全局限流中间件：每小时限流。这里是所有 API （根据 IP）请求加起来。
+		// 作为参考 Github API 每小时最多 60 个请求（根据 IP）。
+		// 测试时，可以调高一点。
+		v1.Use(middlewares.LimitIP("200-H")) //每小时最多200个请求
 		{
-			//1.signup
-			signupController := new(auth.SignupController)
-			// 判断手机号是否已经注册
-			authGroup.POST("/signup/phone/exist", signupController.IsPhoneExist)
-			// 判断email是否已经注册
-			authGroup.POST("signup/email/exist", signupController.IsEmailExist)
-			//用手机号/邮箱注册账号
-			authGroup.POST("/signup/using-phone", signupController.SignupUsingPhone)
-			authGroup.POST("/signup/using-email", signupController.SignupUsingEmail)
+			authGroup := v1.Group("/auth")
+			// 限流中间件：每小时限流，作为参考 Github API 每小时最多 60 个请求（根据 IP）
+			// 测试时，可以调高一点
+			authGroup.Use(middlewares.LimitIP("1000-H"))
+			{
+				//1.signup
+				signupController := new(auth.SignupController)
+				// 判断手机号是否已经注册
+				authGroup.POST("/signup/phone/exist", middlewares.GuestJWT(), signupController.IsPhoneExist)
+				// 判断email是否已经注册
+				authGroup.POST("signup/email/exist", middlewares.GuestJWT(), signupController.IsEmailExist)
+				//用手机号/邮箱注册账号
+				authGroup.POST("/signup/using-phone", middlewares.GuestJWT(), signupController.SignupUsingPhone)
+				authGroup.POST("/signup/using-email", middlewares.GuestJWT(), signupController.SignupUsingEmail)
 
-			// 2.captcha 发送验证码
-			verifyCodeController := new(auth.VerifyCodeController)
-			// 图片验证码，需要加限流
-			authGroup.POST("/verify-codes/captcha", verifyCodeController.ShowCaptcha)
-			authGroup.POST("/verify-codes/phone", verifyCodeController.SendUsingPhone)
-			authGroup.POST("verify-codes/email", verifyCodeController.SendUsingEmail)
+				// 2.verify,verify-code需要限流，邮箱和手机短信限流更严苛，图片验证码限流相对宽松
+				verifyCodeController := new(auth.VerifyCodeController)
+				// 图片验证码
+				authGroup.POST("/verify-codes/captcha", middlewares.LimitPerRoute("50-H"), verifyCodeController.ShowCaptcha)
+				//手机短信和邮箱验证码
+				authGroup.POST("/verify-codes/phone", middlewares.LimitPerRoute("20-H"), verifyCodeController.SendUsingPhone)
+				authGroup.POST("verify-codes/email", middlewares.LimitPerRoute("20-H"), verifyCodeController.SendUsingEmail)
 
-			//3.login
-			loginController := new(auth.LoginController)
-			// 使用手机号，短信验证码进行登录
-			authGroup.POST("/login/using-phone", loginController.LoginByPhone)
-			//使用密码登陆，LoginID可以是phone,email,userName
-			authGroup.POST("login/using-password", loginController.LoginByPassword)
-			//refresh token
-			authGroup.POST("login/refresh-token", loginController.RefreshToken)
+				//3.login
+				loginController := new(auth.LoginController)
+				// 使用手机号，短信验证码进行登录
+				authGroup.POST("/login/using-phone", middlewares.GuestJWT(), loginController.LoginByPhone)
+				//使用密码登陆，LoginID可以是phone,email,userName
+				authGroup.POST("login/using-password", middlewares.GuestJWT(), loginController.LoginByPassword)
+				//
+				//3.2refresh token
+				authGroup.POST("login/refresh-token", middlewares.AuthJWT(), loginController.RefreshToken)
 
-			//4.reset password
-			passwordController := new(auth.PasswordController)
-			//通过手机号和验证码reset密码
-			authGroup.POST("/password-reset/using-phone", passwordController.ResetByPhone)
-			//通过邮箱和验证码reset密码
-			authGroup.POST("/password-reset/using-email", passwordController.ResetByEmail)
+				//4.reset password
+				passwordController := new(auth.PasswordController)
+				//通过手机号和验证码reset密码
+				authGroup.POST("/password-reset/using-phone", middlewares.AuthJWT(), passwordController.ResetByPhone)
+				//通过邮箱和验证码reset密码
+				authGroup.POST("/password-reset/using-email", middlewares.AuthJWT(), passwordController.ResetByEmail)
+			}
+			// v1.GET("/", func(c *gin.Context) {
+			// JSON 格式相应
+			// c.JSON(http.StatusOK, gin.H{
+			// "code":    1,
+			// "message": "this is v1",
+			// })
+			// })
 		}
-		// v1.GET("/", func(c *gin.Context) {
-		// JSON 格式相应
-		// c.JSON(http.StatusOK, gin.H{
-		// "code":    1,
-		// "message": "this is v1",
-		// })
-		// })
+
 	}
 }
