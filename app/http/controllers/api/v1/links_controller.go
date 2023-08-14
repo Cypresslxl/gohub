f577adad53049631d573a835ee0ed4da5b3299a1
Cypresslxl <laixinlianglxl@icloud.com>
2023-08-14 17:08:46 +0800

    Cahce the list of friendly links

--- app/http/controllers/api/v1/links_controller.go
+++ app/http/controllers/api/v1/links_controller.go
@@ -1,10 +1,9 @@
 package v1
 
 import (
+	"github.com/gin-gonic/gin"
 	"gohub/app/models/link"
 	"gohub/pkg/response"
-
-	"github.com/gin-gonic/gin"
 )
 
 type LinksController struct {
@@ -12,6 +11,7 @@ type LinksController struct {
 }
 
 func (ctrl *LinksController) Index(c *gin.Context) {
-	links := link.All()
-	response.Data(c, links)
+	//links := link.All() //从数据库拉数据
+	//response.Data(c, links)
+	response.Data(c, link.AllCached()) //从cache中拉数据
 }
